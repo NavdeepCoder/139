25c3d8a0bc71f88646a642e3c180cd1007c07ef7
NavdeepCoder <121945020+NavdeepCoder@users.noreply.github.com>
2023-05-15 19:54:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function preload() {
+    ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+    missed = loadSound("missed.wav");
+  }
+
+  function setup(){
+    var canvas =  createCanvas(700,600);
+    canvas.parent('canvas');
+  }      
+
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
